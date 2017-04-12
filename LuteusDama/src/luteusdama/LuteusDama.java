5bc98f0995cba86f6fadcd9ae1ca64dbea6fcde2
Luan M. Costa <>
2017-04-11 21:01:36 -0300

    Interface de Login e Cadastro Inicial

---
+++ LuteusDama/src/luteusdama/LuteusDama.java
@@ -0,0 +1,18 @@
+package luteusdama;
+
+import view.TelaLogin;
+
+/**
+ * @author Luan M. Costa
+ */
+public class LuteusDama {
+
+    public static void main(String[] args) {
+        
+        TelaLogin tl = new TelaLogin();
+        tl.setTitle("Login - Luteus");
+        tl.setVisible(true);
+        
+    }
+    
+}
---
+++ LuteusDama/src/models/UsuarioDAO.java
@@ -0,0 +1,5 @@
+package models;
+
+public class UsuarioDAO extends Usuario{
+    
+}
